56c130034ce2870063f8afcf661d16457cbc78ee
Roman Petlitksiy <jeremy_wade@mail.ru>
2024-07-21 21:43:52 +0200

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import reactLogo from './assets/react.svg'
+
+
+function App() {
+
+  return (
+      <div className='container'>
+        <h1>Cheat Sheet Redev</h1>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      
+  )
+}
+
+export default App
